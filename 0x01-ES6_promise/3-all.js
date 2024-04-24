da146365b63e68d1af8c114c3ac935c9b273bf0e
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-04-24 11:11:13 +0000

    3-all.js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,7 @@
+import { uploadPhoto, createUser } from "./utils";
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()]).then((val) => {
+    console.log(`${val[0].body} ${val[1].firstName} ${val[1].lasName}`);
+    }).catch(() => console.log('Signup system offline'));
+}
