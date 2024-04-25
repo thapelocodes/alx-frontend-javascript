d59f51308ba4b52580bafa19a119d394cc87db76
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-04-25 11:12:36 +0000

    hbtn course

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,48 @@
+export default class HolbertonCourse() {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  //getter
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof val !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = val;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(val) {
+    if (typeof val !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(val) {
+    if (!Array.isArray(val)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = val;
+  }
