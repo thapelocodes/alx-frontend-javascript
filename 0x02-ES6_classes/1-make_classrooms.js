d27d632cfcb0491943d5cd163b3a91c6b5450b06
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-04-25 11:01:25 +0000

    make classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,9 @@
 import ClassRoom from './0-classroom.js'
 
 export default function initializeRooms() {
-  let ret = [];
-  ret.push(new ClassRoom(19));
-  ret.push(new ClassRoom(20));
-  ret.push(new ClassRoom(34));
-
-  return ret;
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }
