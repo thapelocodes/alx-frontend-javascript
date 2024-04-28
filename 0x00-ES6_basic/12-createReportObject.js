ef62204aa8b4c741d8540483327dfa72fefed15c
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-04-28 07:04:58 +0000

    12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,6 +1,6 @@
 export default function createReportObject(employeesList) {
   return {
     allEmployees: employeesList,
-    getNumberOfDepartments: () => Object.keys(this.allEmployees).length,
+    getNumberOfDepartments: () => Object.keys(employeesList).length,
   };
 }
