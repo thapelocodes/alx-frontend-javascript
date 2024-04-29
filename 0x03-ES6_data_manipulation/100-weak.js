0d5ee0e7736305554f9ffa0a59817ce599e1f3c1
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-04-29 23:37:46 +0000

    100-weak.js

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,22 @@
+// A weak map of endpoints and the number of calls made.
+export const weakMap = new WeakMap();
+
+const MAX_ENDPOINT_CALLS = 5;
+
+/**
+ * Tracks the number of calls made to an API's endpoint.
+ * @param {{
+ *   protocol: String,
+ *   name: String,
+ * }} endpoint - The endpoint to make a request to.
+ * @author Thapelo M Moumakoe <https://github.com/thapelocodes>
+ */
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+    throw new Error('Endpoint load is high');
+  }
+}
